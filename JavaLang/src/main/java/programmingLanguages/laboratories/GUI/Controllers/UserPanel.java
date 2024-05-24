eb6884f5a0fb073c0cfb6cda5aa533d5ac49c310
yaromochka <yaromochka@yandex.ru>
2024-05-24 17:08:52 +0300

    Сделал панель пользователя

--- JavaLang/src/main/java/programmingLanguages/laboratories/GUI/Controllers/UserPanel.java
+++ JavaLang/src/main/java/programmingLanguages/laboratories/GUI/Controllers/UserPanel.java
@@ -6,13 +6,16 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MultipleSelectionModel;
+import javafx.scene.control.SelectionMode;
 import programmingLanguages.laboratories.GUI.DatabaseHelp.DataBaseDishes;
-import programmingLanguages.laboratories.GUI.DatabaseHelp.Dish;
 import programmingLanguages.laboratories.GUI.HelpMethods.SceneController;
 
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class UserPanel implements Initializable {
@@ -23,11 +26,19 @@ public class UserPanel implements Initializable {
     @FXML
     private ListView<String> listView;
 
+
+    @FXML
+    private Button orderButton;
+
     private ObservableList<String> dishData = FXCollections.observableArrayList();
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        MultipleSelectionModel<String> listViewSelectionModel = listView.getSelectionModel();
+        listViewSelectionModel.setSelectionMode(SelectionMode.MULTIPLE);
+
         /* Кнопка "НАЗАД" */
         backButton.setOnMouseClicked(MouseEvent -> {
             try {
@@ -37,17 +48,33 @@ public class UserPanel implements Initializable {
             }
         });
 
+
         try {
             fillListView();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
+        orderButton.setOnAction(MouseEvent -> {
+            ObservableList<String> selectedDishes = listView.getSelectionModel().getSelectedItems();
+            var dishes = new ArrayList<String>();
+            for (String dish: selectedDishes) {
+                dishes.add(Arrays.asList(dish.split(",")).get(0));
+            }
+            try {
+                deleteFromResidue(dishes);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+
     }
 
 
     public void fillListView() throws SQLException {
         var program = new DataBaseDishes();
-        program.open("jdbc:sqlite:/Users/yaromochka/IdeaProjects/DSTU/JavaLang/src/main/resources/Project/ListOfDishes.db");
+        program.open("jdbc:sqlite:src/main/resources/Project/ListOfDishes.db");
         var resultText = program.getMenu();
 
         while (resultText.next()) {
@@ -59,5 +86,12 @@ public class UserPanel implements Initializable {
 
         // заполняем таблицу данными
         listView.setItems(dishData);
+    }
+
+
+    public void deleteFromResidue(ArrayList<String> dishes) throws SQLException {
+        var program = new DataBaseDishes();
+        program.open("jdbc:sqlite:/Users/yaromochka/IdeaProjects/DSTU/JavaLang/src/main/resources/Project/ListOfDishes.db");
+        program.deleteFrom(dishes);
     }
 }
