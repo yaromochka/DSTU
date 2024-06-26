c0802e7737a1c5e749d78d464baa93860eff206a
yaromochka <yaromochka@yandex.ru>
2024-06-26 19:18:37 +0300

    Почистил лишнее

--- PythonLang/SummerPractSecondCourse/main.py
+++ PythonLang/SummerPractSecondCourse/main.py
@@ -1,5 +1,6 @@
 from des import des
 
+
 def main():
     x = int(input('Вы хотите кодировать(0) или декодировать(1) текст?: '))
     des(x)
@@ -7,13 +8,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# ['331ED0CD8DFC4B17']
-# ['CE9E992FA9C6D70F']
-# ['A16A90162264982B']
-# ['E8CD8BA19CB69C7A']
-# ['0CD00C38B26736DC']
-# ['A16A90162264982B']
-# ['331ED0CD8DFC4B17']
-# ['A16A90162264982B']
